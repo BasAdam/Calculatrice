b1b38c3e02ad28820e962c92b2bbba21c57a05e3
Adam <adam@mac-adam.home>
2023-12-30 18:47:15 +0100

    projet avec commentaires

--- src/main/java/com/mycompany/calculatrice/CalculatorLogic.java
+++ src/main/java/com/mycompany/calculatrice/CalculatorLogic.java
@@ -5,16 +5,19 @@ public class CalculatorLogic {
     private String operator;
 
     public CalculatorLogic() {
+        // Initialisation du résultat à 0 et de l'opérateur à "=" lors de la création de l'instance
         result = 0;
         operator = "=";
     }
 
     public void reset() {
+        // Réinitialisation du résultat à 0 et de l'opérateur à "="
         result = 0;
         operator = "=";
     }
 
     public void calculate(double n) {
+        // Effectuer le calcul en fonction de l'opérateur actuel
         if (operator.equals("+")) {
             result += n;
         } else if (operator.equals("-")) {
@@ -23,30 +26,34 @@ public class CalculatorLogic {
             result *= n;
         } else if (operator.equals("/")) {
             if (n == 0) {
+                // Gérer l'exception de division par zéro
                 throw new ArithmeticException("Erreur : Division par zéro");
             } else {
                 result /= n;
             }
         } else if (operator.equals("=")) {
+            // Si l'opérateur est "=", assigner directement le nombre à result
             result = n;
-        }
-        else if (operator.equals("%")){
+        } else if (operator.equals("%")) {
+            // Calculer le modulo en utilisant l'opérateur "%"
             result %= n;
         }
     }
 
     public void setOperator(String op) {
+        // Définir l'opérateur actuel
         operator = op;
     }
 
     public double getResult() {
+        // Arrondir le résultat à deux décimales si nécessaire
         double roundedResult = Math.round(result * 100.0) / 100.0;
         if (roundedResult == (int) roundedResult) {
+            // Si le résultat est un nombre entier, le renvoyer en tant qu'entier
             return (int) roundedResult;
         } else {
-        
+            // Sinon, renvoyer le résultat avec deux décimales
             return roundedResult;
-
         }
     }
 }
--- src/main/java/com/mycompany/calculatrice/CalculatorUI.java
+++ src/main/java/com/mycompany/calculatrice/CalculatorUI.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class CalculatorUI {
 
     private JFrame frame;
@@ -16,59 +15,65 @@ public class CalculatorUI {
         calculatorLogic = new CalculatorLogic();
         calculating = true;
 
+        // Création de la fenêtre principale
         frame = new JFrame("Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
         frame.setLayout(null);
 
+        // Création du champ de texte pour l'affichage des nombres et des résultats
         display = new JTextField("0");
         display.setBounds(20, 20, 360, 50);
         display.setEditable(false);
         frame.add(display);
 
-        // Creating buttons
+        // Création des libellés des boutons
         String[] buttonLabels = {
             "7", "8", "9", "/",
             "4", "5", "6", "*",
             "1", "2", "3", "-",
-            "0", "C", "%", "=", "+"
+            "0", "C", "%", "=", 
+            "+"
         };
 
         int x = 20, y = 80;
         for (int i = 0; i < buttonLabels.length; i++) {
+            // Création des boutons numériques et d'opération
             JButton button = new JButton(buttonLabels[i]);
             button.setBounds(x, y, 80, 40);
             button.addActionListener(new ActionListener() {
-                
                 public void actionPerformed(ActionEvent e) {
                     String command = e.getActionCommand();
                     if (command.equals("C")) {
+                        // Réinitialisation de l'affichage et de la logique du calcul
                         display.setText("0");
                         calculating = true;
                         calculatorLogic.reset();
                     } 
                     else if (command.equals("=")) {
+                        // Effectuer le calcul et afficher le résultat
                         calculate(Double.parseDouble(display.getText()));
                         display.setText("" + calculatorLogic.getResult());
                         calculating = true;
                     } 
                     else if ("0123456789".contains(command)) {
                         if (calculating) {
+                            // Remplacer l'affichage par le chiffre appuyé
                             display.setText(command);
                         } else {
+                            // Ajouter le chiffre appuyé à l'affichage existant
                             display.setText(display.getText() + command);
                         }
                         calculating = false;
                     } 
-                    
                     else {
-                        
                         if (!calculating) {
+                            // Effectuer un calcul avant d'ajouter un nouvel opérateur
                             calculate(Double.parseDouble(display.getText()));
                             displayResult(calculatorLogic.getResult());
                         }
-
                         calculating = true;
+                        // Définir l'opérateur pour le calcul
                         calculatorLogic.setOperator(command);
                     }
                 }
@@ -82,15 +87,16 @@ public class CalculatorUI {
             }
         }
 
+        // Rendre la fenêtre visible
         frame.setVisible(true);
-
-        
     }
 
     private void calculate(double n) {
         try {
+            // Effectuer le calcul avec la logique de la calculatrice
             calculatorLogic.calculate(n);
         } catch (ArithmeticException e) {
+            // Gérer l'exception de division par zéro
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
                     display.setText("Erreur : Division par zéro");
@@ -98,19 +104,19 @@ public class CalculatorUI {
             });
             return;
         }
+        // Afficher le résultat du calcul
         displayResult(calculatorLogic.getResult());
     }
-    
-    
-    
+
     private void displayResult(double result) {
         if (result == (long) result) {
+            // Si le résultat est un nombre entier, afficher sans décimales
             display.setText(String.format("%d", (long) result));
+            // Remarque : Cette ligne ne sert à rien car la condition n'est pas atteinte
+            // lorsque le bouton "=" est pressé
         } else {
+            // Sinon, afficher le résultat avec deux décimales
             display.setText(String.format("%.2f", result));
         }
-    }    
-
-    
-    
+    }
 }
